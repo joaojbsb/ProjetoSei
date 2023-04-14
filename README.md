a91c13539fabef29ded8ec44ebfc547d250d5499
João Oliveira Lima Júnior <joao.jbsb@gmail.com>
2023-04-14 11:08:18 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,15 @@
+<h1>Portal do sistema SEI para Águas Lindas de Goiás</h1>
+
+<h2>Tecnologias utilizadas</2>
+<ol>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>JavaScript</li>
+    <li>PHP</li>
+    <li>Wordpress</li>
+    <li>MySQL</li>
+</ol>
+
+<div style= 'text-align: center;'>
+<img src="./assets/home.gif">
+</div>
---
+++ javascript/script.js
@@ -0,0 +1,32 @@
+function temaDark() {
+    const dark = document.getElementById('body');
+
+    if (dark.classList.value == '' | dark.classList.value == 'increment-font' | dark.classList.value == 'decrement-font') {
+        dark.classList.add('dark');
+        console.log(dark.classList.value);
+        return
+    }
+    dark.classList.remove('dark');
+
+    console.log(dark.classList)
+};
+
+function incrementFont() {
+    const font = document.getElementById('body')
+    font.classList.remove('decrement-font');
+    font.classList.add('increment-font');
+};
+
+function decrementFont() {
+    const font = document.getElementById('body')
+    font.classList.remove('increment-font');
+    font.classList.add('decrement-font');
+};
+
+function fontPadrao() {
+    font = document.getElementById('body');
+
+    font.classList.remove('increment-font');
+    font.classList.remove('decrement-font');
+}
+
